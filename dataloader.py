Fix a bug with wrong order of arguments

dataloader.py
@@ -5,7 +5,7 @@ import torch
 
 
 class SemanticKITTI(torch.utils.data.Dataset):
-    def __init__(self, root, split="train", kitty_config):
+    def __init__(self, root, kitty_config, split="train"):
         self.files = []
         self.config = kitty_config
         root = Path(root)

train.py
@@ -51,8 +51,8 @@ if __name__ == "__main__":
     ckpt_dir.mkdir(exist_ok=True)
     best_acc = 0.0
 
-    ds_train = SemanticKITTI(args.dataset, "train", kitty_conf)
-    ds_val = SemanticKITTI(args.dataset, "val", kitty_conf)
+    ds_train = SemanticKITTI(args.dataset, kitty_conf, "train")
+    ds_val = SemanticKITTI(args.dataset, kitty_conf, "valid")
     dl_train = torch.utils.data.DataLoader(
         ds_train,
         batch_size=cfg["batch_size"],